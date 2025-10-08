4564c0be72113dd3086564c7034f61217e474118
aldriandn128-eng <aldriandn128@gmail.com>
2025-10-08 14:33:59 +0700

    Update algopro.c

--- algopro.c
+++ algopro.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+
+#define max_booking 50  //ini maksimal orangnya, kalau lebih dari ini program tidak akan mulai atau muncul
+#define max_kamar 5
+
+//ini untuk menyimpan data booking
+struct booking {  
+ char nama[50]
+ int nokamar;
+ int lamaMenginap
+}
+
+//daftar jenis hotel
+char "daftarKamar[max_kamar] = {
+"Kamar 50 - Standart",
+"Kamar 60 - Deluxe",
+"Kamar 70 - Superior",
+"Kamar 80 - Vip",
+"Kamar 90 - Family",
+}
+
+  
